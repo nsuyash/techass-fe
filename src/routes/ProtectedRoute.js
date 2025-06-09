555bf07d2f632a386f09a61202b26a3af62d3028
nsuyash <suyashnandurkar53@gmail.com>
2025-06-09 17:44:44 +0530

    first

--- src/routes/ProtectedRoute.js
+++ src/routes/ProtectedRoute.js
@@ -3,7 +3,7 @@ import { useAuth } from '../context/AuthContext';
 
 const ProtectedRoute = ({ user: requiredRole, children }) => {
   const { user } = useAuth();
-  if (user !== requiredRole) return <Navigate to="/login" />;
+  if (user.role !== requiredRole.role) return <Navigate to="/login" />;
   return children;
 };
 
