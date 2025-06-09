a0843451abbeb777e74f912255c07448ac2243cd
nsuyash <suyashnandurkar53@gmail.com>
2025-06-09 17:35:16 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-// Optional: if you're using React 18+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
