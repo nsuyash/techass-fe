dc384131a4339d022c6dad8e8b5070f99972a60f
nsuyash <suyashnandurkar53@gmail.com>
2025-06-09 17:50:27 +0530

    first

--- src/routes/ProtectedRoute.js
+++ src/routes/ProtectedRoute.js
@@ -1,9 +1,9 @@
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
-const ProtectedRoute = ({ user: requiredRole, children }) => {
+const ProtectedRoute = ({ roles: requiredRole, children }) => {
   const { user } = useAuth();
-  if (user.role !== requiredRole.role) return <Navigate to="/login" />;
+  if (user.role !== requiredRole) return <Navigate to="/login" />;
   return children;
 };
 
